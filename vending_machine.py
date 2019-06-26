02a1c148865fe310d751c420587f459f02ee1e81
Allan <allancc@gmail.com>
2019-06-25 21:29:04 -0400

    Get Balance

--- vending_machine.py
+++ vending_machine.py
@@ -2,9 +2,15 @@ from dataclasses import dataclass, field
 import coins
 import products
 
+#creating our own exception
+class InsufficientFunds(Exception):
+    pass
+
+
 @dataclass
 class VendingMachine:
     inserted_coins: list = field(default_factory=list)
+    purchases: list = field(default_factory=list)
 
     def insert_coin(self, coin):
         if not isinstance(coin, coins.Coin):
@@ -15,3 +21,21 @@ class VendingMachine:
     def buy_product(self, product):
         if not issubclass(product, products.Product):
             raise ValueError
+        balance = self.get_balance() #calling get_balance
+        if balance < product.price:
+            raise InsufficientFunds
+
+        obj = product()
+        self.purchases.append(obj)
+        return obj
+
+    def get_balance(self):
+        balance = 0
+        for coin in self.inserted_coins: #this coin is local so its ok to use
+            #balance = balance + coin.value
+            balance += coin.value
+
+        for product in self.purchases:
+            balance -= product.price
+
+        return balance
